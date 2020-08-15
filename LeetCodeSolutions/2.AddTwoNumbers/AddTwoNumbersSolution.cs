6947908cfe703a734611622fec57058728fa2ce7
Mano Cardozo <manasses.cardozo@gmail.com>
2020-08-15 21:51:23 +1200

    Add two numbers

---
+++ LeetCodeSolutions/2.AddTwoNumbers/AddTwoNumbersSolution.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeSolutions._2.AddTwoNumbers
+{
+    public class AddTwoNumbersSolution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+
+            int left = 0;
+            var values = new List<int>();
+
+            while (l1 != null || l2 != null || left != 0)
+            {
+                var currentValue = (l1?.val ?? 0) + (l2?.val ?? 0) + left;
+
+                if (currentValue > 9)
+                {
+                    left = 1;
+                    currentValue = currentValue - 10;
+                }
+                else
+                {
+                    left = 0;
+                }
+
+                values.Add(currentValue);
+
+                l1 = l1?.next;
+                l2 = l2?.next;
+            }
+
+            ListNode l3 = null;
+
+            for (int i = values.Count - 1; i >= 0; i--)
+            {
+                l3 = new ListNode(values[i], l3);
+            }
+
+            return l3;
+        }
+    }
+}
---
+++ LeetCodeSolutions/2.AddTwoNumbers/ListNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSolutions._2.AddTwoNumbers
+{
+    //Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
