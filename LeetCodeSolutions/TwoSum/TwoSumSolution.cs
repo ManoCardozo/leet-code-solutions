6f15e31dacb2d3eac41cc1686ecf265035c5f1af
Mano Cardozo <manasses.cardozo@gmail.com>
2020-08-15 23:57:29 +1200

    Two sum

---
+++ LeetCodeSolutions/TwoSum/TwoSumSolution.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCodeSolutions.TwoSum
+{
+    public class TwoSumSolution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var firstOperand = 0;
+            var secondOperand = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (i != j)
+                    {
+                        if (nums[i] + nums[j] == target)
+                        {
+                            firstOperand = i;
+                            secondOperand = j;
+                        }
+                    }
+                }
+            }
+
+            return new int[] { firstOperand, secondOperand };
+        }
+    }
+}
